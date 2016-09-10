28d989fa169657778e780250b938b37d434f5366
Forty-Tw0 <jxh130030@utdallas.edu>
2016-09-10 01:13:31 -0500

    Backwards compatability to API level 18 and 21

--- app/src/main/java/lilium/arubabacon/MainActivity.java
+++ app/src/main/java/lilium/arubabacon/MainActivity.java
@@ -1,6 +1,7 @@
 package lilium.arubabacon;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Environment;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bluetooth.*;
@@ -28,6 +30,7 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+    private android.bluetooth.le.ScanCallback ScanCallback;
     BluetoothManager btManager;
     BluetoothAdapter btAdapter;
 
@@ -41,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //android 6.0 requires runtime user permission (api level 23 required...)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (Build.VERSION.SDK_INT >= 23) {
             // Android M Permission check 
             if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 final AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -49,20 +52,73 @@ public class MainActivity extends AppCompatActivity {
                 builder.setMessage("Please grant location access so this app can detect beacons.");
                 builder.setPositiveButton(android.R.string.ok, null);
                 builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @TargetApi(23)
                     public void onDismiss(DialogInterface dialog) {
                         requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
                     }
                 });
                 builder.show();
             }
+
+            String[] PERMISSIONS_STORAGE = new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            // Check if we have write permission
+            int permission = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // We don't have permission so prompt the user
+                requestPermissions(PERMISSIONS_STORAGE, 1);
+            }
         }
 
-        String[] PERMISSIONS_STORAGE = new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-        // Check if we have write permission
-        int permission = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            requestPermissions(PERMISSIONS_STORAGE, 1);
+        if (Build.VERSION.SDK_INT >= 21) {
+            ScanCallback = new android.bluetooth.le.ScanCallback() {
+                @Override
+                @TargetApi(21)
+                public void onScanResult(int callbackType, ScanResult result) {
+                    iBeacon bacon = new iBeacon(result.getDevice(), result.getRssi(), result.getScanRecord().getBytes());
+
+                    if (bacon.isiBeacon) {
+                        if (beacons.contains(bacon)) {
+                            //write to csv for power regression analysis
+                            try {
+                                File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/rssi.csv");
+                                if (!file.exists()) file.createNewFile();
+                                FileOutputStream csv = new FileOutputStream(file, true);
+                                csv.write((result.getDevice().getAddress() + "," + result.getRssi() + "\n").getBytes());
+                                csv.close();
+                            } catch (Exception e) {
+                                System.out.println(e.getMessage());
+                            }
+
+                            //we need historical rssi and interval tracking
+                            //this is a bad way to do it, but I hate object oriented programming
+
+                            long now = System.currentTimeMillis();
+                            bacon.advertInterval = now - beacons.get(beacons.indexOf(bacon)).lastUpdate;
+                            bacon.lastUpdate = now;
+
+                            //lower in this sense means closer to 0 from the negative side
+                            bacon.lowRssi = Math.max(result.getRssi(), beacons.get(beacons.indexOf(bacon)).lowRssi);
+
+                            bacon.cummulativeRssi = beacons.get(beacons.indexOf(bacon)).cummulativeRssi + result.getRssi();
+                            bacon.numRssi = beacons.get(beacons.indexOf(bacon)).numRssi + 1;
+
+                            //lower in this sense means further from 0 from the negative side
+                            bacon.highRssi = Math.min(result.getRssi(), beacons.get(beacons.indexOf(bacon)).highRssi);
+
+                            bacon.pathLoss = ploss;
+
+                            //okay, we have all the data so lets init those distances
+                            bacon.postInit();
+
+                            beacons.set(beacons.indexOf(bacon), bacon);
+                            iBeaconAdapter.notifyDataSetChanged();
+                        } else {
+                            beacons.add(bacon);
+                            iBeaconAdapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+            };
         }
 
         //get and enable BT adapter
@@ -108,11 +164,17 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked)
                 {
-                    btAdapter.getBluetoothLeScanner().startScan(ScanCallback);
-                }
-                else
-                {
-                    btAdapter.getBluetoothLeScanner().stopScan(ScanCallback);
+                    if (Build.VERSION.SDK_INT >= 21) {
+                        btAdapter.getBluetoothLeScanner().startScan(ScanCallback);
+                    } else {
+                        btAdapter.startLeScan(depScanCallback);
+                    }
+                } else {
+                    if (Build.VERSION.SDK_INT >= 21) {
+                        btAdapter.getBluetoothLeScanner().stopScan(ScanCallback);
+                    } else {
+                        btAdapter.stopLeScan(depScanCallback);
+                    }
                 }
             }
         });
@@ -121,52 +183,58 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //device discovery
-    private android.bluetooth.le.ScanCallback ScanCallback = new android.bluetooth.le.ScanCallback(){
+
+    private BluetoothAdapter.LeScanCallback depScanCallback = new BluetoothAdapter.LeScanCallback() {
         @Override
-        public void onScanResult(int callbackType, ScanResult result){
-            iBeacon bacon = new iBeacon(result.getDevice(), result.getRssi(), result.getScanRecord().getBytes());
+        public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    iBeacon bacon = new iBeacon(device, rssi, scanRecord);
 
-            if (bacon.isiBeacon) {
-                if (beacons.contains(bacon)) {
-                    //write to csv for power regression analysis
-                    try {
-                        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/rssi.csv");
-                        if(!file.exists()) file.createNewFile();
-                        FileOutputStream csv = new FileOutputStream(file, true);
-                        csv.write((result.getDevice().getAddress() + "," + result.getRssi() + "\n").getBytes());
-                        csv.close();
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
+                    if (bacon.isiBeacon) {
+                        if (beacons.contains(bacon)) {
+                            //write to csv for power regression analysis
+                            try {
+                                File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/rssi.csv");
+                                if (!file.exists()) file.createNewFile();
+                                FileOutputStream csv = new FileOutputStream(file, true);
+                                csv.write((device.getAddress() + "," + rssi + "\n").getBytes());
+                                csv.close();
+                            } catch (Exception e) {
+                                System.out.println(e.getMessage());
+                            }
+
+                            //we need historical rssi and interval tracking
+                            //this is a bad way to do it, but I hate object oriented programming
+
+                            long now = System.currentTimeMillis();
+                            bacon.advertInterval = now - beacons.get(beacons.indexOf(bacon)).lastUpdate;
+                            bacon.lastUpdate = now;
+
+                            //lower in this sense means closer to 0 from the negative side
+                            bacon.lowRssi = Math.max(rssi, beacons.get(beacons.indexOf(bacon)).lowRssi);
+
+                            bacon.cummulativeRssi = beacons.get(beacons.indexOf(bacon)).cummulativeRssi + rssi;
+                            bacon.numRssi = beacons.get(beacons.indexOf(bacon)).numRssi + 1;
+
+                            //lower in this sense means further from 0 from the negative side
+                            bacon.highRssi = Math.min(rssi, beacons.get(beacons.indexOf(bacon)).highRssi);
+
+                            bacon.pathLoss = ploss;
+
+                            //okay, we have all the data so lets init those distances
+                            bacon.postInit();
+
+                            beacons.set(beacons.indexOf(bacon), bacon);
+                            iBeaconAdapter.notifyDataSetChanged();
+                        } else {
+                            beacons.add(bacon);
+                            iBeaconAdapter.notifyDataSetChanged();
+                        }
                     }
-
-                    //we need historical rssi and interval tracking
-                    //this is a bad way to do it, but I hate object oriented programming
-
-                    long now = System.currentTimeMillis();
-                    bacon.advertInterval = now - beacons.get(beacons.indexOf(bacon)).lastUpdate;
-                    bacon.lastUpdate = now;
-
-                    //lower in this sense means closer to 0 from the negative side
-                    bacon.lowRssi = Math.max(result.getRssi(), beacons.get(beacons.indexOf(bacon)).lowRssi);
-
-                    bacon.cummulativeRssi = beacons.get(beacons.indexOf(bacon)).cummulativeRssi + result.getRssi();
-                    bacon.numRssi = beacons.get(beacons.indexOf(bacon)).numRssi + 1;
-
-                    //lower in this sense means further from 0 from the negative side
-                    bacon.highRssi = Math.min(result.getRssi(), beacons.get(beacons.indexOf(bacon)).highRssi);
-
-                    bacon.pathLoss = ploss;
-
-                    //okay, we have all the data so lets init those distances
-                    bacon.postInit();
-
-                    beacons.set(beacons.indexOf(bacon), bacon);
-                    iBeaconAdapter.notifyDataSetChanged();
-                } else {
-                    beacons.add(bacon);
-                    iBeaconAdapter.notifyDataSetChanged();
                 }
-            }
+            });
         }
     };
 }
